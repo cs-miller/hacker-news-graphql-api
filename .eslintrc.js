e0a217b2b9c6e65a63e62ff0ed4aa3e7dc049e6a
Christopher Miller <millercs624@gmail.com>
2018-04-20 00:52:47 -0400

    prettier eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-  parser: "babel-eslint",
-  plugins: ["flowtype"],
-  extends: ['airbnb-base', "plugin:flowtype/recommended"],
+  parser: 'babel-eslint',
+  plugins: ['flowtype'],
+  extends: ['airbnb-base', 'plugin:flowtype/recommended'],
   rules: {
     'comma-dangle': ['error', 'never'],
     'import/prefer-default-export': false
-  },
+  }
 };
